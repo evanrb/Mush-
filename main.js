b439a8a36a8c8cd463396a51c70d3aa7e1fe1d93
DESKTOP-OVA9NTH\Evan Blank <eblank@ucsc.edu>
2018-08-08 20:46:48 -0700

    Added actual tile map

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-var game = new Phaser.Game(800, 550, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(896, 550, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
     //load atlas
-    game.load.tilemap('map','Assets/embeddedSampleMaze.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map','Assets/level11.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('puppy', 'Assets/puppy.png');
-    game.load.spritesheet('hedge', 'Assets/hedgeTiles.png', 32, 32);
+    game.load.spritesheet('hedgeSheet', 'Assets/hedge tiles 2.png', 32, 32);
         
     var player;
     var p2;
@@ -16,49 +16,53 @@ function create() {
 //add player
    // game.physics.startSystem(Phaser.Physics.ARCADE);
     player = game.add.sprite(100, 300, 'puppy');
-    p2 = game.add.sprite(500, 300, 'puppy');
+    p2 = game.add.sprite(550, 320, 'puppy');
     transformed = false;
     game.world.setBounds(0, 0, 800, 1100);
     
     
     
     map = game.add.tilemap('map');
-    map.addTilesetImage('hedges', 'hedge');
+    map.addTilesetImage('hedges 2', 'hedgeSheet');
+    map.setCollisionByExclusion([]);
     mapLayer = map.createLayer('Tile Layer 1');
     mapLayer.resizeWorld();
     
-    game.physics.enable(game.player);
-    game.physics.enable(game.p2);
-    game.player.collideWorldBounds = true;
+    
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    game.physics.enable(p2, Phaser.Physics.ARCADE);
+    //this.player.collideWorldBounds = true;
 //    
     
 }
 function update() {
     //get user input
-        if(player.alive == true){
+    game.physics.arcade.collide(player, mapLayer);
+    
+        if(player.alive == true ){
             if(game.input.keyboard.justPressed(Phaser.Keyboard.UP)){
-                player.y -= 10;
+                player.y -= 32;
             }
             if (game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT)) {
-                player.x += 10;
+                player.x += 32;
             }
             if(game.input.keyboard.justPressed(Phaser.Keyboard.LEFT)){
-                player.x-=10;
+                player.x-=32;
             }
             if(game.input.keyboard.justPressed(Phaser.Keyboard.DOWN)){
-                player.y+=10;
+                player.y+=32;
             }
             if(game.input.keyboard.justPressed(Phaser.Keyboard.W)){
-                p2.y -= 10;
+                p2.y -= 32;
             }
             if (game.input.keyboard.justPressed(Phaser.Keyboard.D)) {
-                p2.x += 10;
+                p2.x += 32;
             }
             if(game.input.keyboard.justPressed(Phaser.Keyboard.A)){
-                p2.x-=10;
+                p2.x-=32;
             }
             if(game.input.keyboard.justPressed(Phaser.Keyboard.S)){
-                p2.y+=10;
+                p2.y+=32;
             }
         }else{
             if((game.input.keyboard.justPressed(Phaser.Keyboard.UP))&&(game.input.keyboard.isDown(Phaser.Keyboard.W))){
