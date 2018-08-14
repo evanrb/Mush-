0cc80023741eaea6ea123a763c4894974367c18d
DESKTOP-OVA9NTH\Evan Blank <eblank@ucsc.edu>
2018-08-13 21:58:27 -0700

    Added glowflies, bodies, p1-fly collision and glowfly light and animations

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var GamePlay = function(game){
 GamePlay.prototype.preload = function() {
     //load atlas
     game.load.tilemap('map','Assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('RED', 'Assets/Red.png', 32, 56);
+    game.load.spritesheet('RED', 'Assets/Red.png', 32, 47);
     game.load.spritesheet('BLUE', 'Assets/Blue.png',32, 64 );
     game.load.spritesheet('hedgeSheet', 'Assets/hedge tiles 2.png', 32, 32);
     game.load.image('background', 'Assets/level1-background.png');
@@ -20,6 +20,7 @@ GamePlay.prototype.preload = function() {
     var p2;
     var p3;
     var transformed; 
+    var fly;
 //    var shadowTexture;
 //    var lightSprite;
 //    var LIGHT_RADIUS;
@@ -30,44 +31,60 @@ GamePlay.prototype.create = function() {
    // Set stage background color
     this.game.stage.backgroundColor = 0x78453A;
     back = game.add.sprite(0,0, 'background');
-    
-    flyLocations = [
-        [48, 144, 368, 560, 912, 1232, 1264]
-        [112, 80, 240, 784]
-        [144, 432, 1552]
-        [208, 80, 240, 368, 688]
-        [304, 208, 1264, 1392]
-        [336, 688]
-        [368, 1040]
-        [400, 80]
-        [464, 592, 656]
-        [560, 144]
-        [592, 496]
-        [624, 368]
-        [656, 48, 112, 1264]
-        [688, 240, 592, 944]
-        [720, 368]
-        [752, 48, 176]
-        [784, 1136, 1552]
+    console.log
+    var flyLocations = [
+        [48, 144, 368, 560, 912, 1232, 1264],
+        [112, 80, 240, 784],
+        [144, 432, 1552],
+        [208, 80, 240, 368, 688],
+        [304, 208, 1264, 1392],
+        [336, 688],
+        [368, 1040],
+        [400, 80],
+        [464, 592, 656],
+        [560, 144],
+        [592, 496],
+        [624, 368],
+        [656, 48, 112, 1264],
+        [688, 240, 592, 944],
+        [720, 368],
+        [752, 48, 176],
+        [784, 1136, 1552],
         [816, 112, 240, 752, 848]
     ];
 
-    flies = this.game.add.goup();
+    game.world.setBounds(0, 0, 896, 1600);
+    this.flies = this.game.add.group();
+    
+    for(i = 0; i < flyLocations.length; i++){
+        for(j = 1; j < flyLocations[i].length; j++){
+            console.log(i, j);
+            fly = game.add.sprite(flyLocations[i][0], flyLocations[i][j], 'glowfly');
+            fly.animations.add('move', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 7, true);
+            game.physics.enable(fly, Phaser.Physics.ARCADE);
+            fly.anchor.x = .5;
+            fly.anchor.y = .5;
+            fly.body.setSize(15, 15, 8, 8);
+            this.flies.add(fly);
+            fly.animations.play('move');
+        }
+    }
     
 //add player
    // game.physics.startSystem(Phaser.Physics.ARCADE);
     //player = game.add.sprite(100, 300, 'puppy');
-    player = new mushroom(game, 'RED', 1, 48, 48);
+    player = new mushroom(game, 'RED', 1, 48, 46);
     player.anchor.x = .5;
     player.anchor.y = .5;
     game.add.existing(player);
+    
     //p2 = game.add.sprite(550, 320, 'puppy');
     p2 = new mushroom(game, 'BLUE', 2, 848, 48);
     p2.anchor.x = .5;
     p2.anchor.y = .75;
     game.add.existing(p2);
     transformed = false;
-    game.world.setBounds(0, 0, 896, 1600);
+    
   
     map = game.add.tilemap('map');
     map.addTilesetImage('hedges 2', 'hedgeSheet');
@@ -79,13 +96,15 @@ GamePlay.prototype.create = function() {
     game.physics.enable(player, Phaser.Physics.ARCADE);
     game.physics.enable(p2, Phaser.Physics.ARCADE);
     
+    
+    player.body.setSize(32, 32, 0, 15);
     p2.body.setSize(32, 32, 0, 32);
     //this.player.collideWorldBounds = true;
     
     
     // The radius of the circle of light
     this.P1_LIGHT_RADIUS = 70;
-    this.p2_LIGHT_RADIUS = 100;
+    this.p2_LIGHT_RADIUS = 70;
     this.LIGHT_RADIUS = 0;
     
     // Create the shadow texture
@@ -104,13 +123,23 @@ GamePlay.prototype.create = function() {
       
     var playerLight = this.lights.add(player);
     this.lights.add(p2);
+    this.flies.forEach(function(fly){
+        this.lights.add(fly);
+    }, this);
      
     
 };
 GamePlay.prototype.update = function() {
     //get user input
-    game.physics.arcade.collide(player, mapLayer);
-    
+    //game.physics.arcade.collide(player, mapLayer);
+   // game.physics.arcade.collide(player, flies);
+    this.flies.forEach(function(fly){
+        if(game.physics.arcade.collide(player, fly)){
+            fly.exists = false;
+            console.log(fly);
+            this.P1_LIGHT_RADIUS += 5;
+        } 
+    }, this);
     
         if(player.alive == true ){
             if(game.input.keyboard.justPressed(Phaser.Keyboard.UP)){
@@ -153,10 +182,14 @@ GamePlay.prototype.update = function() {
             
         }
     this.updateShadowTexture();
+    
+    
         
     
     if(game.physics.arcade.collide(player, p2) && transformed == false){//player.x == p2.x && player.y == p2.y && transformed == false){
         p3 = game.add.sprite(player.x, player.y, 'puppy');
+        this.lights.add(p3);
+        
         transformed = true;
         player.destroy();
         p2.destroy();
@@ -167,6 +200,9 @@ GamePlay.prototype.render = function(){
     game.debug.body(player);
     game.debug.body(p2);
     game.debug.body(map);
+    this.flies.forEach(function(fly){
+        game.debug.body(fly);
+    }, this);
 };
 GamePlay.prototype.updateShadowTexture = function(){
     this.shadowTexture.context.fillStyle = 'rgb(0, 0, 0)';
@@ -180,10 +216,12 @@ GamePlay.prototype.updateShadowTexture = function(){
         if(light.player === 1){
             //var radius = this.P1_LIGHT_RADIUS + this.game.rnd.integerInRange(1,10);
             this.LIGHT_RADIUS = this.P1_LIGHT_RADIUS;
-        }else(
+        }else if(light.player === 2){
             //var radius = this.p2_LIGHT_RADIUS + this.game.rnd.integerInRange(1,10);
-            this.LIGHT_RADIUS = this.p2_LIGHT_RADIUS
-        )
+            this.LIGHT_RADIUS = this.p2_LIGHT_RADIUS;
+        }else{
+            this.LIGHT_RADIUS = 15;
+        }
         
         var radius = this.LIGHT_RADIUS + this.game.rnd.integerInRange(1,10);
         // Draw circle of light with a soft edge
