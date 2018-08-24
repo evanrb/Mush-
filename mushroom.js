a68bb05dab01ad0fe6655579e5fa477182e6731a
DESKTOP-OVA9NTH\Evan Blank <eblank@ucsc.edu>
2018-08-23 21:26:43 -0700

    Added array motion and collision. thill contains Strange collision error

--- mushroom.js
+++ mushroom.js
@@ -1,6 +1,8 @@
-function mushroom(game, key, playerNum, xPos, yPos){
+function mushroom(game, key, playerNum, xPos, yPos, map){
     
     Phaser.Sprite.call(this, game, xPos, yPos, key);
+    
+    this.maze = map;
     
     var p1R = Phaser.Keyboard.D;
     var p1L = Phaser.Keyboard.A;
@@ -18,13 +20,16 @@ function mushroom(game, key, playerNum, xPos, yPos){
     this.body.collideWorldBounds = true;
     this.anchor.x = .5;
     this.anchor.y = .5;
-    
+    this.mapArrayLocation = [-1, -1];
     if(this.player == 1){
         this.body.setSize(32, 32, 0, 15);
+        this.mapArrayLocation = [1, 1];
     }else if(this.player == 2){
         this.body.setSize(32, 32, 0, 32);
+        this.mapArrayLocation = [1, 27];
     }else if(this.player == 3){
         this.body.setSize(32, 32, 0, 15);
+        this.mapArrayLocation = [14, 14];
     }
     
     if(this.player == 1){
@@ -43,93 +48,81 @@ function mushroom(game, key, playerNum, xPos, yPos){
     }
     
     this.moving = false;
-    this.startMovePosX = this.x;
-    this.startMovePosY = this.y;
-    this.loggedStartPos = false;
-    this.negMovement = false;
 }
 
 mushroom.prototype = Object.create(Phaser.Sprite.prototype);
 mushroom.prototype.constructor = mushroom;
 
-mushroom.prototype.update = function(){
-    
-    var collided = game.physics.arcade.collide(this, mapLayer);
-    
-    if(this.alive && !this.moving ){
-        if(this.player < 3){
-            if(game.input.keyboard.justPressed(this.upInput)){
-                this.body.velocity.y = -32;
-                this.moving = true;
-                this.negMovement = true;
-            }else if(game.input.keyboard.justPressed(this.rightInput)){
-                this.body.velocity.x = 53;
-                this.moving = true;
-                this.negMovement = false;
-            }else if(game.input.keyboard.justPressed(this.leftInput)){
-                this.body.velocity.x = -32;
-                this.moving = true;
-                this.negMovement = true;
-            }else if(game.input.keyboard.justPressed(this.downInput)){
-                this.body.velocity.y = 32;
-                this.moving = true;
-                this.negMovement = false;
+mushroom.prototype.update = function () {
+    if (this.alive && !this.moving) {
+        if (this.player < 3){
+            if (game.input.keyboard.justPressed(this.upInput)) {
+                if (this.maze[this.mapArrayLocation[0] - 1][this.mapArrayLocation[1]] == 0) {
+                    this.legalMove(this.x, this.y - 32, 150);
+                    this.mapArrayLocation[0] -= 1;
+                } else {
+                    this.hitWall(this.x, this.y - 5, 150, this.x, this.y + 5, 150);
+                }
+            } else if (game.input.keyboard.justPressed(this.rightInput)) {
+                if (this.maze[this.mapArrayLocation[0]][this.mapArrayLocation[1] + 1] == 0) {
+                    this.legalMove(this.x + 32, this.y, 150);
+                    this.mapArrayLocation[1] += 1;
+                } else {
+                    this.hitWall(this.x + 5, this.y, 150, this.x - 5, this.y, 150);
+                }
+            } else if (game.input.keyboard.justPressed(this.leftInput)) {
+                if (this.maze[this.mapArrayLocation[0]][this.mapArrayLocation[1] - 1] == 0) {
+                    this.legalMove(this.x - 32, this.y, 150);
+                    this.mapArrayLocation[1] -= 1;
+                } else {
+                    this.hitWall(this.x - 5, this.y, 150, this.x + 5, this.y, 150);
+                }
+            } else if (game.input.keyboard.justPressed(this.downInput)) {
+                if (this.maze[this.mapArrayLocation[0] + 1][this.mapArrayLocation[1]] == 0) {
+                    this.legalMove(this.x, this.y + 32, 150);
+                    this.mapArrayLocation[0] += 1;
+                } else {
+                    this.hitWall(this.x, this.y + 5, 150, this.x, this.y - 5, 150);
+                }
             }
-        }else{
-            if((game.input.keyboard.justPressed(this.p1U))&&(game.input.keyboard.isDown(this.p2U))){
+        }else {
+            if ((game.input.keyboard.justPressed(this.p1U)) && (game.input.keyboard.isDown(this.p2U))) {
                 p3.y -= 32;
-                this.game.camera.y -=32;
-            }else if ((game.input.keyboard.justPressed(p1R))&&(game.input.keyboard.isDown(p2R))) {
+                this.game.camera.y -= 32;
+            } else if ((game.input.keyboard.justPressed(p1R)) && (game.input.keyboard.isDown(p2R))) {
                 p3.x += 32;
-            }else if((game.input.keyboard.justPressed(p1L))&&(game.input.keyboard.isDown(p2L))){
-                p3.x-=32;
-            }else if((game.input.keyboard.justPressed(p1D))&&(game.input.keyboard.isDown(p2D))){
-                    p3.y+=32;
-                    this.game.camera.y +=32;
-            }else if(
-                (game.input.keyboard.justPressed(Phaser.Keyboard.DOWN))&&(!game.input.keyboard.isDown(Phaser.Keyboard.S))||
-                (!game.input.keyboard.justPressed(Phaser.Keyboard.DOWN))&&(game.input.keyboard.isDown(Phaser.Keyboard.S))||
-                (!game.input.keyboard.justPressed(Phaser.Keyboard.UP))&&(game.input.keyboard.isDown(Phaser.Keyboard.W))||
-                (game.input.keyboard.justPressed(Phaser.Keyboard.UP))&&(!game.input.keyboard.isDown(Phaser.Keyboard.W))||
-                (!game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT))&&(game.input.keyboard.isDown(Phaser.Keyboard.D))||
-                (game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT))&&(!game.input.keyboard.isDown(Phaser.Keyboard.D))||
-                (game.input.keyboard.justPressed(Phaser.Keyboard.LEFT))&&(!game.input.keyboard.isDown(Phaser.Keyboard.A))||
-                (!game.input.keyboard.justPressed(Phaser.Keyboard.LEFT))&&(game.input.keyboard.isDown(Phaser.Keyboard.A))
-            ){
-                this.lightRadius -= .06;
-            }
-        }
-        
-    }
-    if(this.moving){
-        if(!this.loggedStartPos){
-            this.startMovePosX = Math.floor(this.x);
-            this.startMovePosY = Math.floor(this.y);
-            this.loggedStartPos = true;
-        }
-        if(!this.negMovement){
-            if(Math.floor(this.x) == this.startMovePosX + 31 || Math.floor(this.y) == this.startMovePosY + 31){
-                this.endMotion();
-            }
-        }else if(this.negMovement){
-            if(Math.floor(this.x) == this.startMovePosX - 31 || Math.floor(this.y) == this.startMovePosY - 31){
-                this.endMotion();
-            }
-        }
-        if(collided){
-            console.log("I made it");
-            console.log(this.x, this.startMovePosX);
-            var tweenBack = game.add.tween(this).to( { x: this.startMovePosX, y: this.startMovePosY }, 150, Phaser.Easing.Linear.None, true);
-            tweenBack.onComplete.add(doSomething, this);function doSomething () {this.endMotion(); console.log("tween complete");}
+            } else if ((game.input.keyboard.justPressed(p1L)) && (game.input.keyboard.isDown(p2L))) {
+                p3.x -= 32;
+            } else if ((game.input.keyboard.justPressed(p1D)) && (game.input.keyboard.isDown(p2D))) {
+                p3.y += 32;
+                this.game.camera.y += 32;
+            } else if (
+                (game.input.keyboard.justPressed(Phaser.Keyboard.DOWN)) && (!game.input.keyboard.isDown(Phaser.Keyboard.S)) ||
+                (!game.input.keyboard.justPressed(Phaser.Keyboard.DOWN)) && (game.input.keyboard.isDown(Phaser.Keyboard.S)) ||
+                (!game.input.keyboard.justPressed(Phaser.Keyboard.UP)) && (game.input.keyboard.isDown(Phaser.Keyboard.W)) ||
+                (game.input.keyboard.justPressed(Phaser.Keyboard.UP)) && (!game.input.keyboard.isDown(Phaser.Keyboard.W)) ||
+                (!game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT)) && (game.input.keyboard.isDown(Phaser.Keyboard.D)) ||
+                (game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT)) && (!game.input.keyboard.isDown(Phaser.Keyboard.D)) ||
+                (game.input.keyboard.justPressed(Phaser.Keyboard.LEFT)) && (!game.input.keyboard.isDown(Phaser.Keyboard.A)) ||
+                (!game.input.keyboard.justPressed(Phaser.Keyboard.LEFT)) && (game.input.keyboard.isDown(Phaser.Keyboard.A))
+                ){this.lightRadius -= .06; }
         }
     }
 }
+
+mushroom.prototype.legalMove = function(xPos, yPos, speed){
+    this.moving = true;
+    var tween = game.add.tween(this).to({ x: xPos, y: yPos }, speed, Phaser.Easing.Linear.None, true);
+    tween.onComplete.add(doSomething, this); function doSomething() { this.endMotion(); }              
+}
+mushroom.prototype.hitWall = function(xPos, yPos, speed, endXPos, endYPos, endSpeed){
+    this.moving = true;
+    var tween = game.add.tween(this).to({ x: xPos, y: yPos }, speed, Phaser.Easing.Linear.None, true);
+    tween.onComplete.add(doSomething, this); function doSomething() {
+        var tweenBack = game.add.tween(this).to({ x: endXPos, y: endYPos }, endSpeed, Phaser.Easing.Linear.None, true);
+        tweenBack.onComplete.add(moveBack, this); function moveBack() { this.endMotion(); }
+    }
+}
 mushroom.prototype.endMotion = function(){
-    this.body.velocity.y = 0;
-    this.body.velocity.x = 0;
-    this.x = Math.floor(this.x);
-    this.y = Math.floor(this.y);
-    this.loggedStartPos = false;
     this.moving = false;
-    console.log(this.x, this.y);
 }
